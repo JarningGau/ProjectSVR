1106b2afeb7bb3930b2585e8d7c781e9e2b0f19b
Jarning Gau <jarninggau@gmail.com>
2023-06-20 16:21:15 +0800

    feat: add LabelTransfer() function

--- R/wrappers.R
+++ R/wrappers.R
@@ -1,10 +1,11 @@
 #' @include projection.R
+#' @include label_transfer.R
 NULL
 
 #' A wrapper for mapping query data onto reference.
 #'
 #' @param seu.q A query Seurat object.
-#' @param reference Reference model
+#' @param reference Reference model.
 #' @param assay.q The assay used for reference mapping.
 #' @param ncores Number of threads for calculation.
 #' @return A Seurat object. The projected reference embeddings were saved in a
@@ -58,5 +59,66 @@ MapQuery <- function(seu.q, reference, assay.q = "RNA", ncores = 1) {
                              do.norm = "L2", cores = ncores)
   seu.q[["UCell"]] <- Seurat::CreateAssayObject(data = gss.mat)
   seu.q[["ref.umap"]] <- Seurat::CreateDimReducObject(proj.obj@embeddings, key = "refUMAP_", assay = assay.q)
+  return(seu.q)
+}
+
+#' A wrapper for transferring reference cell labels to the query via KNN method.
+#'
+#' @param seu.q A query Seurat object.
+#' @param reference Reference model.
+#' @param reduction.q Reduction name storing projected reference embeddings. Default is 'ref.umap'.
+#' @param ref.emb.col The column names storing the reference embeddings in reference model.
+#' @param ref.label.col The column name storing the cell labels to be transferred.
+#' @param k The K param for KNN model. Default is 10.
+#'
+#' @return A Seurat object. The predicted cell type is store in 'knn.pred.celltype' column.
+#' @export
+#'
+LabelTransfer <- function(seu.q, reference, reduction.q = "ref.umap",
+                          ref.emb.col = paste0("UMAP_", 1:2),
+                          ref.label.col = "cluster.name", k = 10) {
+  # 检查输入的 seu.q 是否为 Seurat 对象
+  if(!inherits(seu.q, "Seurat")){
+    stop("Input 'seu.q' must be a Seurat object.")
+  }
+  if (!(reduction.q %in% Reductions(seu.q))) {
+    stop("Input 'reduction.q' does not exist in seu.q.")
+  }
+
+  # 检查输入的 reference 是否包含指定的元素
+  required_elements <- c("ref.cellmeta")
+  missing_elements <- setdiff(required_elements, names(reference))
+  if(length(missing_elements) > 0){
+    stop(paste0("Input 'reference' is missing the following elements: ",
+                paste(missing_elements, collapse = ", ")))
+  }
+
+  # 检查输入的 ref.emb.col 的长度是否合法
+  if(length(ref.emb.col) != 2){
+    stop("Input 'ref.emb.col' must contain two column names for embedding coordinates.")
+  }
+
+  # 检查输入的 ref.label.col 是否在 reference$ref.cellmeta 中存在
+  if(!(ref.label.col %in% colnames(reference$ref.cellmeta))){
+    stop("Input 'ref.label.col' does not exist in reference$ref.cellmeta.")
+  }
+
+  # 检查输入的 k 是否为正整数
+  if(!is.numeric(k) || !k %% 1 == 0 || k <= 0){
+    stop("Input 'k' must be a positive integer.")
+  }
+  query.emb <- seu.q[[reduction.q]]@cell.embeddings
+  ref.cellmeta <- reference$ref.cellmeta$meta.data
+  ref.emb <- ref.cellmeta[, ref.emb.col]
+  ref.labels <- ref.cellmeta[[ref.label.col]]
+  names(ref.labels) <- rownames(ref.cellmeta)
+  knn.pred <- KnnLabelTransfer(query.emb = query.emb, ref.emb = ref.emb, ref.labels = ref.labels, k = k)
+  seu.q$knn.pred.celltype <- knn.pred$labels
+  seu.q$knn.pred.votes <- knn.pred$votes
+  seu.q$knn.pred.perc <- knn.pred$perc
+  ref.celltype.levels <- levels(ref.cellmeta[[ref.label.col]])
+  if (!is.null(ref.celltype.levels)) {
+    seu.q$knn.pred.celltype <- factor(seu.q$knn.pred.celltype, levels = ref.celltype.levels)
+  }
   return(seu.q)
 }
