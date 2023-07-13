b586238b2a98d8b79ce2b7a31de8f57b51122be5
Jarning Gau <jarninggau@gmail.com>
2023-07-13 09:55:25 +0800

    feat: add ribosomal gene set

---
+++ R/data.R
@@ -0,0 +1,5 @@
+#' A data set containing all human ribosomal genes
+#'
+#' @format A vector with 184 elements:
+#' @source \url{https://www.gsea-msigdb.org/gsea/msigdb/cards/GOCC_RIBOSOME.html}
+"ribo.genes"
